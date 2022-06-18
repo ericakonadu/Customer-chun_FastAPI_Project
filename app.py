1924d3e08d122038345f0ef0d4b9ace688ac1eab
Ifeanyi Nneji <ifeanyinneji777@gmail.com>
2022-06-18 17:10:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import pickle
 import uvicorn
+import joblib
 
 app = FastAPI(debug=True)
 
@@ -18,7 +19,8 @@ def Info(name:str):
 @app.post('/predict')   
 def Customer(CreditScore: int, Geography:int, Gender: int, Age : int, Tenure:int, Balance:int, NumOfProduct:int,HasCrCard:int, IsActiveMember:int, EstimatedSalary:int):
     
-    model = pickle.load(open('C:/Users/User/Herokuapp/Customer_chun/chun_model.pkl','rb'))
+    #model = pickle.load(open('C:/Users/User/Herokuapp/Customer_chun/chun_model.pkl','rb'))
+    model = joblib.load("chun_model.pkl")
     make_prediction = model.predict(([[CreditScore,Geography, Gender, Age, Tenure, Balance, NumOfProduct, HasCrCard, IsActiveMember, EstimatedSalary]]))
     result = make_prediction[0]
     if (result > 0.5):
@@ -30,6 +32,6 @@ def Customer(CreditScore: int, Geography:int, Gender: int, Age : int, Tenure:int
     #     'The prediction is {}'.format(result)
     # }   
     
-if __name__ == "__main__":
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+#if __name__ == "__main__":
+#    uvicorn.run(app, host='127.0.0.1', port=8000)
 
